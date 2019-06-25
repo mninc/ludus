aacd912fe903ef6d202c15abfcf4c0521933e7a3
mninc <manicminer106@gmail.com>
2019-06-25 20:13:48 +0100

    clean up connect 4

--- connect4.py
+++ connect4.py
@@ -104,10 +104,11 @@ def init(bot, data):
     async def connect4(ctx, user: discord.User = None):
         board = [["none" for _ in range(6)] for _ in range(7)]
         if user and not user.bot:
-            await ctx.send(user.mention + ": " + ctx.author.display_name + " has invited you to play connect four. Type 'play' to confirm")
+            await ctx.send(user.mention + ": " + ctx.author.display_name +
+                           " has invited you to play connect four. Type 'play' to confirm.")
             message = await get_reply(ctx, 30, channel_user=user)
             if not message or message.content.lower() != "play":
-                await ctx.send("user did not confirm")
+                await ctx.send(ctx.user + ": " + user.display_name + " did not confirm.")
                 return
             players = [ctx.author, user]
         else:
@@ -124,14 +125,16 @@ def init(bot, data):
                         if not column:
                             other = await other_player(player_number, players)
                             if other != "bot":
-                                await ctx.send(player.display_name + " did not respond.\n" + other.mention + ": You win!")
+                                await ctx.send(player.display_name + " did not respond.\n" + other.mention +
+                                               ": You win!")
                             else:
                                 await ctx.send(player.display_name + " did not respond.\nBot wins!")
                             return
                         if len(column.content) == 1 and column.content.isdigit():
                             column = int(column.content) - 1
                             if board[column][0] != "none":
-                                await ctx.send(player.mention + ": That column is full! Please enter a number between 1 and 7.")
+                                await ctx.send(player.mention +
+                                               ": That column is full! Please enter a number between 1 and 7.")
                             else:
                                 break
                         else:
@@ -169,12 +172,3 @@ def init(bot, data):
                         await render_board(ctx, board)
                         await ctx.send(player.mention + ": You won!")
                     return
-                    
-                    
-                    
-                    
-                    
-                
-        
-            
-
