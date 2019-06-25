027ca249f98e7b2e733364cd674415ca8669ffdc
sayersauce <sayer_max@yahoo.co.uk>
2019-06-25 14:27:46 +0100

    Update textadventure.py

--- textadventure.py
+++ textadventure.py
@@ -6,6 +6,28 @@
 #         \/          \/          \/
 
 
+class Player:
+    alive = True
+    choice = 0
+
+    def __init__(self, name, user):
+        self.name = name
+        self.user = user
+        self.squad = ""
+
+    async def set_squad(self, squad):
+        self.squad = squad
+
+    async def set_name(self, new_name):
+        self.name = new_name
+
+    async def die(self):
+        self.alive = False
+
+    async def set_choice(self, new_choice):
+        self.choice = new_choice
+
+
 def init(bot, data):
     @bot.command()
     async def textadv(ctx, *args):
@@ -19,3 +41,9 @@ def init(bot, data):
 async def begin(ctx):
     user = ctx.message.author
     await user.send("begin game")
+    player = Player(user.name, user)
+
+
+async def ask_squad(ctx, player):
+    await player.user.send("--A load of hypesquad lore etc--")
+    await player.user.send("Choose your HypeSquad")
