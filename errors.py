a5f1c90353d81b8e3558adf6555fb8676b36c03b
mninc <manicminer106@gmail.com>
2019-06-26 16:05:51 +0100

    catch errors

---
+++ errors.py
@@ -0,0 +1,12 @@
+from discord.ext.commands import errors
+
+
+def init(bot, data):
+    @bot.event
+    async def on_command_error(ctx, error):
+        if type(error) is errors.MissingRequiredArgument:
+            await ctx.send(ctx.author.mention +
+                           ": You are missing a required argument for this command.\nCheck >help for more information.")
+        else:
+            await ctx.send(ctx.author.mention + ": An error occurred executing this command. This has been logged.")
+            print(error)
