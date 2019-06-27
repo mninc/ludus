1842b836fa49222f0e0bd81710a8d8c96374aaef
mninc <manicminer106@gmail.com>
2019-06-27 12:17:22 +0100

    update the website with config file instructions

--- util/reddit.py
+++ util/reddit.py
@@ -1,10 +1,9 @@
 import praw
 import json
 
-with open("config_reddit.json") as f:
+with open("config.json") as f:
     config = json.load(f)
 
-reddit = praw.Reddit(client_id=config["id"],
-                     client_secret=config["secret"],
-                     user_agent=config["user_agent"])
-
+reddit = praw.Reddit(client_id=config["reddit_id"],
+                     client_secret=config["reddit_secret"],
+                     user_agent=config["reddit_user_agent"])
