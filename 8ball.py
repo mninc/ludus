2a7e8a78d086d850169a04ec0db460390c2b7053
sayersauce <sayer_max@yahoo.co.uk>
2019-06-28 18:15:59 +0100

    Update 8ball.py

--- 8ball.py
+++ 8ball.py
@@ -5,5 +5,5 @@ def init(bot, data):
     @bot.command(aliases=["8ball"])
     async def eightball(ctx, user: discord.User):
         if user:
-            with open("images/8ball.png", "rb") as picture:
+            with open("images/8Ball.png", "rb") as picture:
                 await ctx.send(user.mention, file=discord.File(picture, "8ball.png"))
