906df65efb76b9bb1ba578757184003f33a357ff
mninc <manicminer106@gmail.com>
2019-06-28 12:04:29 +0100

    youtube video search/trending

---
+++ getvideo.py
@@ -0,0 +1,41 @@
+import aiohttp
+import json
+from random import choice
+
+
+with open("config.json") as f:
+    config = json.load(f)
+
+
+def init(bot, data):
+    @bot.command()
+    async def trending(ctx, country_code="US"):
+        if len(country_code) != 2:
+            await ctx.send(ctx.author.mention + ": Country code must be two characters long!")
+            return
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://www.googleapis.com/youtube/v3/videos", params={
+                "part": "id",
+                "chart": "mostPopular",
+                "regionCode": country_code.upper(),
+                "maxResults": 25,
+                "key": config["youtube_api_key"]
+            }) as response:
+                r = await response.json()
+                video = choice(r["items"])
+                await ctx.send("https://youtube.com/watch?v=" + video["id"])
+    
+    @bot.command(aliases=["yt"])
+    async def youtube(ctx, query):
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://www.googleapis.com/youtube/v3/search", params={
+                "part": "id",
+                "maxResults": 1,
+                "order": "relevance",
+                "q": query,
+                "type": "video",
+                "key": config["youtube_api_key"]
+            }) as response:
+                r = await response.json()
+                video = r["items"][0]["id"]["videoId"]
+                await ctx.send("https://youtube.com/watch?v=" + video)
